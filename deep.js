06314f89ba009edb0b9186b54e752eedb8f9b729
Nikkei96 <nikitarudentsov@yandex.ru>
2021-02-12 18:36:53 +0300

    :robot: - reduce

--- deep.js
+++ deep.js
@@ -21,3 +21,17 @@ function filter(arr, filter) {
 
 const expression_2 = filter([1, 2, 3, 4, 5], el => el % 2 == 0)
 console.log('Массив с filter на проверку каждого элемента массива на чётность:', expression_2) // ➝ [2, 4]
+// -------------------------------------
+
+// #3. Функция reduce для массива
+function reduce(arr, combine, start) {
+  let current = start
+  arr.forEach(el => {
+    current = combine(el, current)
+  })
+  return current
+}
+
+const expression_3 = reduce([1, 2, 3, 4, 5], (el, current) => el + current, 0)
+console.log('Массив с reduce на сложение всех чисел массива: ', expression_3)
+// -------------------------------------
